760158e3110f9a1f25a57308790ea7426a4b155d
Kenshin-Y <kenshin121026@gmail.com>
2023-03-03 02:30:26 +0900

    update

--- Math/Basicmath.cpp
+++ Math/Basicmath.cpp
@@ -1,72 +1,63 @@
 /*
-    @created:2020-9
-    @description:
+    @ created:2020-9
+    @ updated:2023-3
+    @ description:
         * gcd: O(logN)
         * lcm: O(logN)
         * powmod: O(logN)
         * my_pow: O(logN) modなし
-        * extgcd: O(logN) ax+by=1なる(x,y)
-        *       : ax+by=gcd(a,b)なら存在していて、この時a/=gcd,b/=gcdを渡せば良い
+        * extgcd: O(logN) 参照渡しすることで，ax+by=gcd(a,b) なる (x,y) が格納される．
 */
-#ifndef BASICMATH
-#define BASICMATH
 #include <bits/stdc++.h>
 using namespace std;
 
-long long gcd(long long a, long long b)
-{
-    if(b==0) return a;
-    return gcd(b,a%b);
+long long gcd(long long a, long long b){
+    if(b == 0) return a;
+    return gcd(b, a%b);
 }
 
-long long lcm(long long a, long long b)
-{
+long long lcm(long long a, long long b){
     return a/gcd(a,b)*b;
 }
 
-long long powmod(long long x, long long n, long long MOD)
-{
-    long long ret=1;
-    while(n>0){
+long long powmod(long long x, long long n, long long MOD){
+    long long ret = 1;
+    while(n > 0){
         if(n&1){
-            ret*=x;
-            ret%=MOD;
-            n-=1;
+            ret *= x;
+            ret %= MOD;
+            n -= 1;
         }else{
-            x*=x;
-            x%=MOD;
-            n>>=1;
+            x *= x;
+            x %= MOD;
+            n >>= 1;
         }
     }
     return ret;
 }
 
-long long my_pow(long long x, long long n)
-{
+long long my_pow(long long x, long long n){
     long long ret=1;
-    while(n>0){
+    while(n > 0){
         if(n&1){
-            ret*=x;
-            n-=1;
+            ret *= x;
+            n -= 1;
         }else{
-            x*=x;
-            n>>=1;
+            x *= x;
+            n >>= 1;
         }
     }
     return ret;
 }
 
-// ax + by = 1
-int extgcd(int a, int b, int &x, int &y)
-{
-    int d=a;
-    if(b!=0){
-        d=extgcd(b,a%b,y,x);
+int extgcd(int a, int b, int &x, int &y){
+    int d = a;
+    if(b != 0){
+        d = extgcd(b, a%b, y, x);
         y -= (a/b)*x;
     }else{
-        x=1; y=0;
+        x = 1;
+        y = 0;
     }
     return d;
 }
-
-#endif // BASICMATH
